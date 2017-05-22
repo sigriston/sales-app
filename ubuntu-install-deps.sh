1c995b1ba6e283d164e0a7ab35b6cc93bea7db0d
Thiago Sigrist <sigrist@gmail.com>
2017-05-22 14:53:59 -0300

    script instalação ubuntu deps

---
+++ ubuntu-install-deps.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+# Ubuntu dependency install script
+# Tested with Ubuntu 16.04
+
+sudo apt install build-essential libsqlite3-0 libsqlite3-dev nodejs ruby ruby-dev zlib1g zlib1g-dev
+
+sudo gem install bundler
