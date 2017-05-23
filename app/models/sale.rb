d31533b686e6ed956ec78f65d853972e2dbafaf2
Thiago Sigrist <sigrist@gmail.com>
2017-05-23 10:49:09 -0300

    lógica de criação normalizada (bulk_create_row)

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,4 +2,35 @@ class Sale < ApplicationRecord
   belongs_to :customer
   belongs_to :product
   validates :quantity, presence: true
+
+  def total
+    product.price * quantity
+  end
+
+  def self.grandtotal
+    joins(:product).sum("price * quantity")
+  end
+
+  def self.create_row(row)
+    customer = Customer.find_or_create_by_retry(name: row[:customer])
+
+    vendor = Vendor.find_or_create_by_retry(name: row[:vendor]) do |v|
+      v.address = row[:address]
+    end
+
+    product = Product.find_or_create_by_retry(description: row[:description],
+                                              vendor: vendor) do |p|
+      p.price = row[:price]
+    end
+
+    create(customer: customer, product: product, quantity: row[:quantity])
+  end
+
+  def self.bulk_create_row(rows)
+    ActiveRecord::Base.transaction do
+      rows.map do |row|
+        create_row(row)
+      end
+    end
+  end
 end
--- test/models/sale_test.rb
+++ test/models/sale_test.rb
@@ -6,6 +6,24 @@ class SaleTest < ActiveSupport::TestCase
     @alt_product = products(:two)
     @customer = customers(:one)
     @alt_customer = customers(:two)
+    @rows = [
+      {
+        customer: "Chance Predovic III",
+        description: "ASUS ROG GeForce GTX 1080 STRIX-GTX1080-A8G-GAMING 8GB",
+        price: 569.99,
+        quantity: 1,
+        address: "Apt. 277 717 Anastasia Tunnel, Adellaville, NH 20403",
+        vendor: "Huel LLC"
+      },
+      {
+        customer: "Miss Felicia Dickens",
+        description: "EVGA GeForce GTX 1080 Ti FE 11G-P4-6390-KR 11GB",
+        price: 699.99,
+        quantity: 2,
+        address: "58127 Boyer Mountains, Creminberg, IL 76309-7412",
+        vendor: "Hane-Wiegand"
+      }
+    ]
   end
 
   test "add new sale" do
@@ -46,6 +64,50 @@ class SaleTest < ActiveSupport::TestCase
                              product: @product,
                              quantity: 4)
       assert alt_sale.persisted?
+    end
+  end
+
+  test "total price for one sale" do
+    sale = sales(:one)
+    assert_equal 23.97, sale.total
+  end
+
+  test "grandtotal for all sales" do
+    assert_equal 78.72, Sale.grandtotal
+  end
+
+  test "create_row" do
+    sale = Sale.create_row(@rows[0])
+    assert sale.persisted?
+    assert sale.customer.persisted?
+    assert sale.product.persisted?
+    assert sale.product.vendor.persisted?
+
+    assert_equal @rows[0][:customer], sale.customer.name
+    assert_equal @rows[0][:description], sale.product.description
+    assert_equal @rows[0][:price], sale.product.price
+    assert_equal @rows[0][:quantity], sale.quantity
+    assert_equal @rows[0][:address], sale.product.vendor.address
+    assert_equal @rows[0][:vendor], sale.product.vendor.name
+  end
+
+  test "bulk_create_row" do
+    sale_list = Sale.bulk_create_row(@rows)
+
+    assert_not_empty sale_list
+
+    for sale, row in sale_list.zip(@rows)
+      assert sale.persisted?
+      assert sale.customer.persisted?
+      assert sale.product.persisted?
+      assert sale.product.vendor.persisted?
+
+      assert_equal row[:customer], sale.customer.name
+      assert_equal row[:description], sale.product.description
+      assert_equal row[:price], sale.product.price
+      assert_equal row[:quantity], sale.quantity
+      assert_equal row[:address], sale.product.vendor.address
+      assert_equal row[:vendor], sale.product.vendor.name
     end
   end
 end
